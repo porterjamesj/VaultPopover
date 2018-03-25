aa28c08b026a082a2d5eba481fb7a6a00daa8d56
James Porter <james@jamesporter.me>
2018-03-25 18:31:02 -0400

    Initial Commit

---
+++ VaultPopover/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VaultPopover
+//
+//  Created by James on 3/25/18.
+//  Copyright © 2018 porterjamesj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
